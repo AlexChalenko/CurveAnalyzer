8acb8d0e8caf3145e6d504e10a2d6dcc45c0b334
Alexey Work@Home <21961404+AlexChalenko@users.noreply.github.com>
2020-03-19 22:50:13 +0100

    improvements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,7 +7,10 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -18,13 +21,21 @@ namespace CurveAnalyzer
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        readonly string DataUrl = "https://iss.moex.com/iss/engines/stock/zcyc.xml?date={0}&iss.only=yearyields&iss.meta=off";
+        private readonly string DataUrl = "https://iss.moex.com/iss/engines/stock/zcyc.xml?date={0}&iss.only=yearyields&iss.meta=off";
         //readonly string OldDataUrl = https://iss.moex.com/iss/engines/state/markets/zcyc.json
 
         public SeriesCollection SeriesCollection { get; private set; }
         public ObservableCollection<string> LabelsX { get; set; }
 
+        public Func<double, string> XFormatter { get; set; }
         public IssData IssData { get; set; }
+        private int daysBack;
+        private string lastUpdateDate;
+        private Double minValue = double.MaxValue;
+        private double maxValue = double.MinValue;
+
+        private DispatcherTimer timer;
+
         public string LastUpdateDate
         {
             get
@@ -42,19 +53,25 @@ namespace CurveAnalyzer
         public MainWindow()
         {
             LabelsX = new ObservableCollection<string>();
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Curve",
-                    Values = new ChartValues<double>(),
-                }
-            };
+            SeriesCollection = new SeriesCollection();
 
+            XFormatter = x => x.ToString("000.00000");
             DataContext = this;
             InitializeComponent();
+
+            MainChart.LegendLocation = LegendLocation.Right;
+            CalendarControl.SelectedDate = DateTime.Now;
+            LastUpdateDate = CalendarControl.SelectedDate.Value.ToShortDateString();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 1, 0);
+            timer.Tick += onTimerTick;
         }
 
+        private void onTimerTick(object sender, EventArgs e)
+        {
+            updateChart(DateTime.Today);
+
+        }
 
         public Task<bool> GetData(DateTime date)
         {
@@ -77,6 +94,10 @@ namespace CurveAnalyzer
                     IssData = data;
                     tcs.TrySetResult(true);
                 }
+                else
+                {
+                    tcs.TrySetResult(false);
+                }
             }
             catch (Exception ex)
             {
@@ -86,49 +107,133 @@ namespace CurveAnalyzer
             return tcs.Task;
         }
 
-        int daysBack;
-        private string lastUpdateDate;
-
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private void updateChart(DateTime dateTime)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+            LastUpdateDate = dateTime.ToShortDateString();
+
+            GetDataButton.IsEnabled = false;
+            GetData(dateTime).ContinueWith(task =>
+            {
+
+                LabelsX.Clear();
+                //SeriesCollection[0].Values.Clear();
+
+                if (task.IsCanceled || task.Exception != null)
+                {
+                }
+                else
+                {
+                    var data = IssData.data.rows.Select(x => x.value).ToArray();
+                    var labels = IssData.data.rows.Select(x => x.period).ToArray();
+
+                    double max = data.Max();
+                    double min = data.Min();
+
+                    minValue = Math.Min(minValue, min);
+                    maxValue = Math.Max(maxValue, max);
+                    minValue = Math.Floor(minValue / 0.25d) * 0.25d;
+                    maxValue = Math.Ceiling(maxValue / 0.25d) * 0.25d;
+
+                    bool isDataNew = true;
+                    int index = -1;
+
+                    foreach (var item in SeriesCollection)
+                    {
+                        if (item.Title == LastUpdateDate)
+                        {
+                            isDataNew = false;
+                            index = SeriesCollection.IndexOf(item);
+                        }
+                    }
+
+                    if (isDataNew)
+                    {
+                        SeriesCollection.Add(new LineSeries
+                        {
+                            Values = new ChartValues<double>(),
+                            Title = LastUpdateDate.ToString()
+                        });
+                        var currentIndex = SeriesCollection.CurrentSeriesIndex;
+                        foreach (var item in data)
+                        {
+                            SeriesCollection[currentIndex].Values.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < data.Length; i++)
+                        {
+                            double item = data[i];
+                            SeriesCollection[index].Values[i] = item;
+                        }
+                    }
+                    LabelsX.Clear();
+                    foreach (var item in labels)
+                    {
+                        LabelsX.Add(item.ToString());
+                    }
+
+                    //SeriesCollection[0].Values.Add(item.value);
+                    //}
+
+                    var axis = MainChart.AxisY[0];
+                    axis.MaxValue = maxValue;
+                    axis.MinValue = minValue;
+                    //Separator separator = new Separator { Step = 0.25d, IsEnabled = false };
+                    //axis.Separator = separator;
+                    GetDataButton.IsEnabled = true;
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
         private void GetData_Click(object sender, RoutedEventArgs e)
         {
-            DateTime calcDay;
+            if (CalendarControl.SelectedDate.HasValue)
+                updateChart(CalendarControl.SelectedDate.Value);
+        }
 
-            do
+        private void CalendarControl_SelectedDatesChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ToggleButton.IsChecked = false;
+        }
+
+        private void MainChart_DataClick(object sender, ChartPoint chartPoint)
+        {
+            //MyPopsup.IsOpen = false;
+            //MyPopsup.IsOpen = true;
+        }
+
+        private void DeleteChart_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+
+            if (checkBox.IsChecked.HasValue && checkBox.IsChecked.Value)
             {
-                daysBack--;
-                calcDay = DateTime.Now.AddDays(daysBack);
-
-            } while (calcDay.DayOfWeek == DayOfWeek.Saturday || calcDay.DayOfWeek == DayOfWeek.Sunday);
-
-            LastUpdateDate = calcDay.ToShortDateString();
-
-            GetData(calcDay).ContinueWith(task =>
+                timer.Start();
+            }
+            else if (timer.IsEnabled)
             {
-                LabelsX.Clear();
-                SeriesCollection[0].Values.Clear();
+                timer.Stop();
+            }
+        }
 
-                if (task.IsCanceled || task.Exception != null)
-                {
-
-                }
-                else
-                {
-                    double max = IssData.data.rows.Select(x => x.value).Max() * 1.01;
-                    double min = IssData.data.rows.Select(x => x.value).Min() * .99;
-
-                    foreach (var item in IssData.data.rows)
-                    {
-                        SeriesCollection[0].Values.Add(item.value);
-                        LabelsX.Add(item.period.ToString());
-                    }
-                }
-            }, TaskScheduler.Current);
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in SeriesCollection)
+            {
+                //item.Values.Clear();
+                //item.Values.Remove();
+                
+            }
+            SeriesCollection.Clear();
         }
     }
 }
